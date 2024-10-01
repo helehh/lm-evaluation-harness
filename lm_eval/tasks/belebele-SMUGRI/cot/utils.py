5b59ba2eab8967631a170fda3800640549fd7f89
helehh <kuulmets@gmail.com>
2024-10-01 15:43:08 +0300

    add belebele-smugri

---
+++ lm_eval/tasks/belebele-SMUGRI/cot/utils.py
@@ -0,0 +1,148 @@
+
+'''
+Let's think step by step.
+The premise "L'uomo aprì il rubinetto." can be translated from Estonian into English as "The man turned on the faucet."
+The first option "Il gabinetto si riempì d'acqua." can be translated as "The toilet filled with water.", whereas the second option "Dell'acqua fluì dal beccuccio." can be translated as "Water flowed from the spout."
+If the man turned on the faucet then it makes sense that water flowed from the spout as a result.
+Therefore, the answer is "Dell'acqua fluì dal beccuccio.".
+'''
+
+_vro ={
+    "flores_passage": "Kõgõ vahtsõmba bülletääni perrä näütsiq merepinna tasõmõ andmõq, et tegüsi tsunami. Pago Pago ja Niue lähkün löüti kimmit tsunami tüütämise tunnussit. Tongan olõ-õi teedäq ant suurõmbist kah’ostuisist ei vigastuisist, a aotlidsõlt kattõ ärq eelektri, nii et tuuperäst saa-as Tonga võimuq kätte PTWC edesiantut tsunamihoiatust. Hawaiil panti terves kolmapääväs kinniq 14 kuuli, miä ommaq mere veeren vai tuu lähkün, huulmalda tuust, et hoiatusõq võediq tagasi.",
+    "question": "Kuimuudu eelektri ärqminek Tongat seo jutujupi perrä mõot'?",
+    "mc_answer1": "Oll' mõni teedüs kah'ostuisist",
+    "mc_answer2": "Võimuq es saaq teedüst, et kõik jälq kõrran om",
+    "mc_answer3": "Meretasõt saa-as mõõtaq",
+    "mc_answer4": "Võimuq es saaq tähtsät hoiatust kätte",
+    "correct_answer": "D"
+    }
+
+_liv={
+     "flores_passage": "Amā ūd bulletīn pierrõ mierpīnda kuordit dattõd nägțiztõ, ku sugīz tsunami. Vȯļ lieudtõd pīldzi tsunami tiegū Pago Pago ja Niue ležgõl. Tongasõ äb ūo tieutõd iļ sūŗimõd kajtõkst äb ka iļ viggõ sōmizt, agā vȯļ irg ku elektrīb kadīz, sīepierāst Tonga iz sō käddõ PTWC sōtõd tsunamikuondõkst. Hawaiilõ paņtõ amā kuolmõndpǟvaks vizzõ 14 skūolõ, mis ātõ rāndas agā sīe ležgõl, koks kuondõkst võtīztõ tāgiž.",
+    "question": "Sīe rīndakub pierrõ, kui elektrīb kaddimi mȯjtiz Tongõ?",
+    "mc_answer1": "Tieutizt iļ mingizt kajtõkst",
+    "mc_answer2": "Vāldamīed äb sōtõt tīetõ iļ kuondõkst järā võtāmiz",
+    "mc_answer3": "Mier kuordit aigimi vȯļ mȯjtõd",
+    "mc_answer4": "Vāldamīed äb sōtõt vajāgliži kuondõkši",
+    "correct_answer": "D"
+}
+
+_kpv = {
+    "flores_passage": "Бӧръя висьталӧм серти, саридз вылна бӧрся видзӧдан мыччӧдъяс петкӧдлісны цунамилысь матыстчӧм. Тыдалана му вӧрӧм вӧлі пасйӧма Паго-Паго да Ниуэ дорын. Тонгаысь эз волыны юӧръяс ён киссьӧмъяс либӧ доймалӧмъяс йылысь, но вӧлісны кад кежлӧ би кусӧдӧм, мый, мыччӧдъяс серти, оз сет позянлун Тонгалӧн веськӧдлысьяслы босьтны цунами йылысь юӧртӧмсӧ, кодӧс ыстіс Лӧнь отваса цунамияс йылысь юӧртан шӧрин (PTWC). Гаваи вылын дзонь Шӧрлун вылӧ тупкысьӧма дас нёль школа, кодъяс вӧлісны вадорын либӧ сылы матын, юӧртӧмсӧ дугӧдӧм вылӧ видзӧдтӧг.",
+    "question": "Гижӧд серти, кыдзи би кусӧдӧмыс тӧдчис Тонга вылын?",
+    "mc_answer1": "Воисны юӧръяс кымынкӧ киссьӧмъяс йылысь",
+    "mc_answer2": "Веськӧдлысьяслы эз во юӧр юӧртӧмъяс дугӧдӧм йылысь.",
+    "mc_answer3": "Саридз вылна вежсьӧмъяс тӧдчисны мыччӧдъяс вылын.",
+    "mc_answer4": "Веськӧдлысьяслы эз воны колана юӧртӧмъяс.",
+    "correct_answer": "D"
+}
+
+_est = {
+    "flores_passage": "Uusima bülletääni kohaselt näitasid merepinna taseme andmed, et moodustus tsunami. Pago Pago ja Niue lähistel tuvastati kindlasti tsunami tegevust. Tongas pole teatatud suurematest kahjustustest ega vigastustest, kuid ajutiselt kadus elekter, mistõttu ei saanud Tonga võimud kätte PTWC edastatud tsunamihoiatust. Hawaiil suleti terveks kolmapäevaks 14 kooli, mis asuvad rannikul või selle lähedal, hoolimata sellest, et hoiatused võeti tagasi.",
+    "question": "Vastavalt sellele lõigule, kuidas mõjutas elektrikatkestus Tongat?",
+    "mc_answer1": "Teatati mõningatest kahjustustest",
+    "mc_answer2": "Võimud ei saanud teadet hoiatuste eemaldamise kohta",
+    "mc_answer3": "Meretaseme mõõtmine oli mõjutatud",
+    "mc_answer4": "Võimud ei saanud vajalikke hoiatusi",
+    "correct_answer": "D"
+}
+
+_fin = {
+    "flores_passage": "Uusimman tiedotteen mukaan meren pinnan korkeuslukemat osoittavat tsunamin syntyneen. Lähellä Pago Pagoa ja Niuea havaittiin ilmeistä tsunamitoimintaa. Tonga ei ole ilmoittanut suurista aineellisista tai henkilövahingoista, mutta sähköt katkesivat väliaikaisesti, mikä tiettävästi esti Tongan viranomaisia saamasta Tyynenmeren tsunamivaroituskeskuksen lähettämää tsunamivaroitusta. Neljä havaijilaiskoulua rantaviivalla tai lähellä rantaviivaa suljettiin koko keskiviikoksi, vaikka varoitukset poistettiin.",
+    "question": "Miten sähköjen katkeaminen vaikutti Tongaan tekstikatkelman mukaan?",
+    "mc_answer1": "Joistakin vahingoista ilmoitettiin",
+    "mc_answer2": "Viranomaiset eivät saaneet uutisia perutuista varoituksista",
+    "mc_answer3": "Sillä oli vaikutuksia meren pinnan korkeuslukemiin",
+    "mc_answer4": "Viranomaiset eivät saaneet asiaankuuluvia varoituksia",
+    "correct_answer": "D"
+}
+
+_eng = {
+    "flores_passage": "According to the latest bulletin, sea level readings indicated a tsunami was generated. There was some definite tsunami activity recorded near Pago Pago and Niue. No major damage or injuries have been reported in Tonga, but power was temporarily lost, which reportedly prevented Tongan authorities from receiving the tsunami warning issued by the PTWC. Fourteen schools in Hawaii located on or near coastlines were closed all of Wednesday despite the warnings being lifted.",
+    "question": "According to the passage, how was Tonga affected by the power outage?",
+    "mc_answer1": "There was some damage reported",
+    "mc_answer2": "Authorities did not receive news regarding the lifted warnings",
+    "mc_answer3": "Sea level readings were affected",
+    "mc_answer4": "Authorities were unable to receive relevant warnings",
+    "correct_answer": "D"
+}
+
+CoT_example_vro = f'''P: {_vro["flores_passage"]}
+Q: {_vro["question"]}
+A. {_vro["mc_answer1"]}
+B. {_vro["mc_answer2"]}
+C. {_vro["mc_answer3"]}
+D. {_vro["mc_answer4"]}
+Answer: Let's think step by step.
+The passage "{_vro['flores_passage']}" can be translated from Võro to English as "{_eng['flores_passage']}".
+The question "{_vro['question']}" can be translated from Võro to English as "{_eng['question']}" and the options translate as follows: "A. {_eng['mc_answer1']}", "B. {_eng['mc_answer2']}", "C. {_eng['mc_answer3']}", "D. {_eng['mc_answer4']}". According to text the correct answer in English is "D. {_eng['mc_answer4']}". Therefore, the answer is "D. {_vro['mc_answer4']}".'''
+
+
+CoT_example_liv = f'''P: {_liv["flores_passage"]}
+Q: {_liv["question"]}
+A. {_liv["mc_answer1"]}
+B. {_liv["mc_answer2"]}
+C. {_liv["mc_answer3"]}
+D. {_liv["mc_answer4"]}
+Answer: Let's think step by step.
+The passage "{_liv['flores_passage']}" can be translated from Livonian to English as "{_eng['flores_passage']}".
+The question "{_liv['question']}" can be translated from Livonian to English as "{_eng['question']}" and the options translate as follows: "A. {_eng['mc_answer1']}", "B. {_eng['mc_answer2']}", "C. {_eng['mc_answer3']}", "D. {_eng['mc_answer4']}". According to text the correct answer in English is "D. {_eng['mc_answer4']}". Therefore, the answer is "D. {_liv['mc_answer4']}".'''
+
+
+CoT_example_kpv = f'''P: {_kpv["flores_passage"]}
+Q: {_kpv["question"]}
+A. {_kpv["mc_answer1"]}
+B. {_kpv["mc_answer2"]}
+C. {_kpv["mc_answer3"]}
+D. {_kpv["mc_answer4"]}
+Answer: Let's think step by step.
+The passage "{_kpv['flores_passage']}" can be translated from Komi to English as "{_eng['flores_passage']}".
+The question "{_kpv['question']}" can be translated from Komi to English as "{_eng['question']}" and the options translate as follows: "A. {_eng['mc_answer1']}", "B. {_eng['mc_answer2']}", "C. {_eng['mc_answer3']}", "D. {_eng['mc_answer4']}". According to text the correct answer in English is "D. {_eng['mc_answer4']}". Therefore, the answer is "D. {_kpv['mc_answer4']}".'''
+
+
+CoT_example_fin = f'''P: {_fin["flores_passage"]}
+Q: {_fin["question"]}
+A. {_fin["mc_answer1"]}
+B. {_fin["mc_answer2"]}
+C. {_fin["mc_answer3"]}
+D. {_fin["mc_answer4"]}
+Answer: Let's think step by step.
+The passage "{_fin['flores_passage']}" can be translated from Finnish to English as "{_eng['flores_passage']}".
+The question "{_fin['question']}" can be translated from Finnish to English as "{_eng['question']}" and the options translate as follows: "A. {_eng['mc_answer1']}", "B. {_eng['mc_answer2']}", "C. {_eng['mc_answer3']}", "D. {_eng['mc_answer4']}". According to text the correct answer in English is "D. {_eng['mc_answer4']}". Therefore, the answer is "D. {_fin['mc_answer4']}".'''
+
+
+CoT_example_est = f'''P: {_est["flores_passage"]}
+Q: {_est["question"]}
+A. {_est["mc_answer1"]}
+B. {_est["mc_answer2"]}
+C. {_est["mc_answer3"]}
+D. {_est["mc_answer4"]}
+Answer: Let's think step by step.
+The passage "{_est['flores_passage']}" can be translated from Estonian to English as "{_eng['flores_passage']}".
+The question "{_est['question']}" can be translated from Estonian to English as "{_eng['question']}" and the options translate as follows: "A. {_eng['mc_answer1']}", "B. {_eng['mc_answer2']}", "C. {_eng['mc_answer3']}", "D. {_eng['mc_answer4']}". According to text the correct answer in English is "D. {_eng['mc_answer4']}". Therefore, the answer is "D. {_est['mc_answer4']}".'''
+
+
+
+def _doc_to_text(doc):
+    return f'P: {doc["flores_passage"]}\nQ: {doc["question"]}\nA. {doc["mc_answer1"]}\nB. {doc["mc_answer2"]}\nC. {doc["mc_answer3"]}\nD. {doc["mc_answer4"]}\nAnswer:'
+
+def doc_to_text_cot_oneshot_vro(doc):
+    question = _doc_to_text(doc)
+    return CoT_example_vro + "\n\n" + question
+
+def doc_to_text_cot_oneshot_liv(doc):
+    question = _doc_to_text(doc)
+    return CoT_example_liv + "\n\n" + question
+
+def doc_to_text_cot_oneshot_kpv(doc):
+    question = _doc_to_text(doc)
+    return CoT_example_kpv + "\n\n" + question
+
+def doc_to_text_cot_oneshot_est(doc):
+    question = _doc_to_text(doc)
+    return CoT_example_est + "\n\n" + question
+
+
+def doc_to_text_cot_oneshot_fin(doc):
+    question = _doc_to_text(doc)
+    return CoT_example_fin + "\n\n" + question
