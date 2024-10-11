f8e7521808fe0acfe5a70a6319c79274f1c53aef
helehh <kuulmets@gmail.com>
2024-10-11 10:07:16 +0300

    add estQA

---
+++ lm_eval/tasks/EstQA/utils.py
@@ -0,0 +1,57 @@
+
+_fewshot_samples = [
+    {
+            "context": "Üldist teenistuskohustust asuti riikides kodanikele kehtestama peamiselt 19. sajandil. Toonane taristu ja tehnika (sh raudtee, laevandus, side, raskerelvastus; hiljem juba ka autod ja lennundus) kiire areng võimaldas koondada sõja pidamiseks suuri sõjaväelaste ja vahendite hulki ning paisata neid kiirelt ja ootamatult pikkade vahemaade taha. Elukutseliste sõjaväelaste piiratud arvukus osutus taolisteks operatsioonideks või nendele vastamiseks ebapiisavaks ja tuli leida lisa üldise sunniviisilise teenistuskohustuse kehtestamisega. Lisaks osutus nii võimalikuks anda kodanikele sõjalist ettevalmistust, et neid oleks vajadusel sõja korral võimalik reservväelasina mobiliseerida ja kiirelt rakendada.",
+            "question": "Millal sai ajateenistus alguse?",
+            "answers": [ { "answer_start": 73, "text": "19. sajandil" } ],
+            "title": "Ajateenistus",
+            "id": "9e477685-8c71-4ab0-90b1-bd0d74aaccc2"
+        }
+        ,
+        {
+            "context": 'Apollonil oli Kreekas kaks kultuskeskust: Delfi ja Delos, mis mõlemad pärinevad 8. sajandist eKr. Delosel austati eelkõige Apolloni kaksikõde Artemist. Delfi ja Delose Apolloni kultused olid sedavõrd erinevad, et ühes piirkonnas võisid lähestikku olla mõlema pühamud. Delfis võis Apolloni oraaklikultus olla juba Mükeene kultuuri aegadel. Minoilisele päritolule viitab ka Delfi preestrite hilisem nimetus Labryaden ehk "kaksikkirve kandjad" – λάβρυς oli Kreeta labürindi püha sümbol. Ühes nn Homerose hümnidest kandis Apollon delfiinina preestrid Kreetalt Delfisse, ning Apollon Delphinios oli Kreetal ja teistel saartel austatud merejumal. Apolloni õde Artemist seostati Minose kultuuri "loomade emanda" Britomartisega.',
+            "question": "Millal alustati Delfis Apolloni kummardamist?",
+            "answers": [ { "answer_start": 313, "text": "Mükeene kultuuri aegadel" } ],
+            "title": "Apollon",
+            "id": "6925b111-c6c2-4f7f-9fbd-6761d56dcb0e"
+        },
+        {
+            "context": "Rusizi jõe määrasid oma alade piiriks Belgia ja Saksamaa 11. augustil 1910 allkirjastatud leppega. Aastal 1924 määras Belgia ja Suurbritannia vaheline protokoll Tanganjika ja Rwanda-Urundi piiri kohal, kus praegu on Burundi, Rwanda ja Tansaania piiri kolmikpunkt Mwibu ja Kagera voolunõvade liitumiskohal. Pärast Esimest maailmasõda anti Kisaka ehk Kissaga ringkond (praegu Gisaka ringkond) Rwanda kirdeosas lühikeseks ajaks Briti mandaadi alla, et saaks rajada läbi Briti alade raudtee Ugandasse. Samuti anti Bugufi piirkond Ruvuvu ja Kagera jõe vahel praegusest Burundi-Tansaania piirist läänes Tanganjika osaks.",
+            "question": "Kelle kontrolli alla läks pärast Esimest maailmasõda praegune Gisaka ringkond?",
+            "answers": [ { "answer_start": 425, "text": "Briti" } ],
+            "title": "Burundi",
+            "id": "aa5fc084-7888-4480-b04b-c4d6ade74b9f"
+        }
+        ,
+        {
+            "context": "Hoone Promenaadi uulits 15 ehitati algselt 1911. aastal, kuid 1944. aasta märtsipommitamise käigus suur osa hoonest hävis ja tänapäevane hoone on üles ehitatud esialgsetel müüridel. Ehitise esimesel korrusel asuvad eri laiusega ümarkaaraknad pärinevad hoone algsest välimusest. Estonia puiestee 15 asusid Kaubamaja V. J. Gorbatschev & Pojad, aastatel 1919–1924 (algselt Viruvärava puiestee 15) Eesti Tarbijateühistute Keskühistu autoosakond, 1930–1932 (algselt Estonia puiestee 15/17) Gustav Hammeri autotöökoda ja 1932–1935 Vladimir Johansoni auto-, pleki- ja vasetöökoda ning seejärel J. Puhk & Pojad raua- ja koloniaalosakond.",
+            "question": "Mis juhtus Promenaadi 15 hoonega märtsipommitamise ajal?",
+            "answers": [ { "answer_start": 99, "text": "suur osa hoonest hävis" } ],
+            "title": "Estonia puiestee",
+            "id": "801e5473-21df-441e-9330-0f40d46bec42"
+        }
+        ,
+        {
+            "context": "Suurema osa Gröönimaast moodustab enam-vähem puutumatu loodus. Üle 80% Gröönimaast on kaetud igijää ja igilumega, mille paksus on kuni 2300 meetrit. Saare keskosas on jääalune maapind kohati üle 300 m allpool Maailmamere taset. Jäävabu alasid on kokku 410 449 km² (umbes 9 korda suurem ala kui Eesti). Gröönimaa kirdeosa hõlmab Põhja- ja Ida-Gröönimaa rahvuspark, mis on maailma suurim looduskaitseala (972 000 km²). Sellest tohutust alast veidi lõuna poole saare idarannikule jääb ka kõigi Taani Kuningriigi valduste kõrgeim mäetipp Gunnbjørn Fjeld – 3694 m. Valdav osa Gröönimaa mägedest jääb siiski alla 1500 meetri.",
+            "question": "Kui suur osa Gröönimaast on aastaringselt jää ja lume all?",
+            "answers": [ { "answer_start": 63, "text": "Üle 80%" } ],
+            "title": "Gröönimaa",
+            "id": "224cc8e2-f138-4dc8-ae75-ea8b4cba1c8f"
+        }
+]
+
+_eng = {
+    "context": "General conscription began to be imposed on citizens in countries mainly in the 19th century. The rapid development of infrastructure and technology at the time (including railways, shipping, communications, heavy weaponry; later also cars and aviation) made it possible to amass large numbers of soldiers and resources for warfare and deploy them quickly and unexpectedly over long distances. The limited number of professional soldiers proved insufficient for such operations or to respond to them, and it became necessary to supplement them by establishing mandatory conscription. Additionally, this made it possible to provide citizens with military training, so that in case of war, they could be mobilized as reservists and quickly deployed.",
+    "question": "When did conscription begin?",
+    "title": "conscription",
+    "answers": [ { "answer_start": 73, "text": "in the 19th century" } ],
+    "id": "9e477685-8c71-4ab0-90b1-bd0d74aaccc2"
+}
+
+def list_fewshot_samples() -> list[dict]:
+    return _fewshot_samples
+
+
+def doc_to_text_cot_oneshot(doc):
+    _est = _fewshot_samples[0]
+    return f'Title: {_est["title"]}\nPassage: {_est["context"]}\nQuestion: {_est["question"]}\nAnswer: Let\'s think step by step. The passage {_est["context"]} can be translated from Estonian to English as "{_eng["context"]}" and the question "{_est["question"]}" can be translated as "{_eng["question"]}". The answer in English is "{_eng["answers"][0]["text"]}". Therefore, the answer is "{_est["answers"][0]["text"]}"\n\nTitle: {doc["title"]}\nPassage: {doc["context"]}\nQuestion: {doc["question"]}\nAnswer:'
